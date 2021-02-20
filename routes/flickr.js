5827bec64e909d211a34f5d4b3db3e15145ca969
Julhend <meetredmist@gmail.com>
2021-02-20 16:45:25 +0700

    edit key

--- routes/flickr.js
+++ routes/flickr.js
@@ -9,7 +9,7 @@ app.get('/feeds', async (req, res) => {
     const baseRecent = `https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&`
     const baseSearch = `https://www.flickr.com/services/rest/?method=flickr.photos.search&`
     const { page, tags } = req.query
-    const api_key = process.env.FLICKR_KEY
+    const api_key = `4ef5475c8e91c9452a786f1160e025a4`
     const format = `json`
     const nojsoncallback = `1`
     const query = qs.stringify({ api_key, ...req.query, format, nojsoncallback })
